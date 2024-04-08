b2e7849c337a5879328ace8e710c39d91c328ab6
Chen <cchengraphic@gmail.com>
2024-04-07 20:00:57 -0700

    add products in home screen

--- frontend/src/components/admin/AdminProducts.jsx
+++ frontend/src/components/admin/AdminProducts.jsx
@@ -1,9 +1,47 @@
 import { Button } from "react-bootstrap";
 import NewProduct from "./NewProduct";
+import { useGetProductsQuery } from "../../slices/productsApiSlice";
 
 const AdminProducts = () => {
+  const { data: products, isLoading } = useGetProductsQuery();
+  console.log(products);
   return (
     <div>
+      <h1>Admin Products</h1>
+      {isLoading && <p>Loading...</p>}
+      {!isLoading && !products && <p>No products found</p>}
+
+      {products && (
+        <div>
+          <p>{products.length} products found</p>
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Category</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <tr key={product._id}>
+                  <td>{product._id}</td>
+                  <td>
+                    <img src={product.image} alt={product.name} className="w-100" />
+                  </td>
+                  <td>{product.name}</td>
+                  <td>{product.price}</td>
+                  <td>{product.category}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+
+   
       <Button className="btn btn-primary">Create Product</Button>
       <NewProduct />
     </div>
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import { useGetProductsQuery } from "../slices/productsApiSlice";
 
 const HomeScreen = () => {
-  return <div>HomeScreen</div>;
+  const { data: products, isLoading } = useGetProductsQuery();
+  return (
+    <div>
+      <h2>All Products</h2>
+      {isLoading && <p>Loading...</p>}
+      {products && (
+        <div>
+          <p>{products.length} products found</p>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default HomeScreen;
