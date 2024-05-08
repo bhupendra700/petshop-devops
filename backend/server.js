a75a5a51319a43ac6dc01260813bffd0f5b7202a
Chen <cchengraphic@gmail.com>
2024-05-07 21:55:54 -0700

    Prepare For Production

--- backend/server.js
+++ backend/server.js
@@ -35,9 +35,17 @@ app.get("/api/config/paypal", (req, res) =>
 const __dirname = path.resolve(); // set __dirname to the absolute path of the directory containing the source file
 app.use("/uploads", express.static(path.join(__dirname, "/uploads"))); // make the uploads folder static
 
-app.get("/", (req, res) => {
-  res.send("Api is running...");
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api is running...");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
