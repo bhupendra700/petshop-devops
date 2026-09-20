d26329133aa289c4a6efe5da2b1e3c0950212a37
bhupendra700 <bhupendra7021@gmail.com>
2026-09-20 14:16:24 +0530

    Testing

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -5,13 +5,11 @@ const generateToken = (res, userId) => {
     expiresIn: "30d",
   });
 
-  // set JWT token as HTTP only cookie
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    // secure: true, // only works on https
+    secure: false,
     sameSite: "strict",
-    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
 
