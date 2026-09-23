7a9dc706d4b9b0d8855a590c290319fbf9a94a3e
bhupendra700 <bhupendra7021@gmail.com>
2026-09-23 19:10:37 +0530

    Adding mongodb

--- terraform/modules/mongodb/outputs.tf
+++ terraform/modules/mongodb/outputs.tf
@@ -1,4 +1,13 @@
 output "connection_string" {
-  value     = mongodbatlas_advanced_cluster.this.connection_strings.standard_srv
+  value = format(
+    "mongodb+srv://%s:%s@%s/petshop",
+    urlencode(var.database_username),
+    urlencode(var.database_password),
+    trimprefix(
+      mongodbatlas_advanced_cluster.this.connection_strings.standard_srv,
+      "mongodb+srv://"
+    )
+  )
+
   sensitive = true
 }
