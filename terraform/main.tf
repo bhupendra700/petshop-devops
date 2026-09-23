ad59de8d45840620a177125c453cd84bba97b7e6
bhupendra700 <bhupendra7021@gmail.com>
2026-09-23 15:03:48 +0530

    Adding mongodb

--- terraform/main.tf
+++ terraform/main.tf
@@ -66,15 +66,7 @@ resource "local_file" "ansible_inventory" {
     ${module.ec2.private_ec2_instance_ids[0]}
     ${module.ec2.private_ec2_instance_ids[1]}
 
-    [mongodb]
-    ${module.ec2.private_ec2_instance_ids[2]}
-
     [apps:vars]
-    ansible_connection=amazon.aws.aws_ssm
-    ansible_aws_ssm_region=us-east-1
-    ansible_aws_ssm_bucket_name=petshop-ansible-ssm-temp-bucket
-
-    [mongodb:vars]
     ansible_connection=amazon.aws.aws_ssm
     ansible_aws_ssm_region=us-east-1
     ansible_aws_ssm_bucket_name=petshop-ansible-ssm-temp-bucket
