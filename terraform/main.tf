31c4839b4602fe8498ca44160f398c6010187970
bhupendra700 <bhupendra7021@gmail.com>
2026-09-23 15:31:30 +0530

    Adding mongodb

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ module "vpc" {
   public_subnet_name   = ["petshop_public_subnet_1", "petshop_public_subnet_2"]
   public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnet_name  = ["petshop_private_subnet_1", "petshop_private_subnet_2"]
-  private_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
+  private_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
   availability_zone    = ["us-east-1a", "us-east-1b"]
   public_rt_name       = "simple_petshop_public_rt"
   private_rt_name      = "simple_petshop_private_rt"
