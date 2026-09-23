ea838307d51a5c00b8bf25e1f1e3f9a33c78a49c
bhupendra700 <bhupendra7021@gmail.com>
2026-09-23 18:41:35 +0530

    Adding mongodb

--- terraform/modules/mongodb/outputs.tf
+++ terraform/modules/mongodb/outputs.tf
@@ -1,4 +1,4 @@
 output "connection_string" {
-  value     = mongodbatlas_advanced_cluster.this.connection_strings[0].standard_srv
+  value     = mongodbatlas_advanced_cluster.this.connection_strings.standard_srv
   sensitive = true
 }
