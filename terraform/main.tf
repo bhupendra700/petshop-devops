5b411810cca8f91c54b4f51f650d0d5f81726c0e
bhupendra700 <bhupendra7021@gmail.com>
2026-09-26 15:16:10 +0530

    Cleaning the TF code

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,21 +71,12 @@ data "aws_secretsmanager_secret" "backend" {
   name = "petshop/prod/backend"
 }
 
-data "aws_secretsmanager_secret_version" "backend" {
-  secret_id = data.aws_secretsmanager_secret.backend.id
-}
-
 resource "aws_secretsmanager_secret_version" "backend" {
   secret_id = data.aws_secretsmanager_secret.backend.id
 
-  secret_string = jsonencode(
-    merge(
-      jsondecode(data.aws_secretsmanager_secret_version.backend.secret_string),
-      {
-        MONGO_URI = module.mongodb.connection_string
-      }
-    )
-  )
+  secret_string = jsonencode({
+    MONGO_URI = module.mongodb.connection_string
+  })
 }
 
 resource "local_file" "ansible_inventory" {
